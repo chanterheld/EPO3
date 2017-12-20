c962284ca9cf616f5fec130b4cbdda75b6a6c6b5
chanterheld <chanterheld@gmail.com>
2017-12-20 16:12:18 +0100

    vga

--- VHDL/Source_files/vga/adr_to_color/score/adr_to_color_sc_arch2.vhd
+++ VHDL/Source_files/vga/adr_to_color/score/adr_to_color_sc_arch2.vhd
@@ -1,24 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity adr_to_color_sc is
-	port(	clk	: in	std_logic;
-		reset	: in	std_logic;
-		game_rst: in	std_logic;
-		plus_one: in	std_logic;
-		x_adr	: in	std_logic_vector(1 downto 0);
-		y_adr	: in	std_logic_vector(3 downto 0);
-
-		max	: out	std_logic;
-		e_n	: out	std_logic;
-		color	: out	std_logic_vector(1 downto 0)
-		
-	);
-end entity adr_to_color_sc;
-
-library ieee;
-use ieee.std_logic_1164.all;
-
 
 architecture low_lvl of adr_to_color_sc is
 --component score_cnt is
