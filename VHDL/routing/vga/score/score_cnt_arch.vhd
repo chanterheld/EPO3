f3173601fac0566389f491b62b198b58cde27097
chanterheld <chanterheld@gmail.com>
2017-12-22 20:21:36 +0100

    vga_routing

--- VHDL/routing/vga/score/score_cnt_arch.vhd
+++ VHDL/routing/vga/score/score_cnt_arch.vhd
@@ -29,7 +29,7 @@ tens_0: up_cnt_cell port map(clk, tens_r, ones_max, tens(0), uo2_int);
 
 --l_ones: up_one_cnt_4 port map(clk, ones_r, plus_one, ones_s);
 ones_3: t_ff port map(clk, ones_r, uo4_int(3), ones_s(3));
-ff_gen:
+ones_gen:
 for i in 0 to 2 generate
 	ones: up_cnt_cell port map(clk, ones_r, uo4_int(i), ones_s(i), uo4_int(i+1));
 end generate;
