904b43a0155e3b91d33b3f61ddea651b6fef5936
supermsi <34647843+supermsi@users.noreply.github.com>
2018-01-12 10:05:51 +0000

    Update memory_top_lvl_arch.vhd

--- VHDL/Source_files/storage/memory_top_lvl_arch.vhd
+++ VHDL/Source_files/storage/memory_top_lvl_arch.vhd
@@ -59,8 +59,8 @@ port (	clk : in std_logic;
 );
 end component;
 
-signal vga_flag_s, vga_clr_flag, ctrl_flag_s, ctrl_ap_flag, fal_write, ctrl_clr_flag, fsm_out, a_flag, input_sel, storage_out, reset_s : std_logic;
-signal rng_color, return_color : std_logic_vector(1 downto 0);
+signal vga_flag_s, vga_clr_flag, ctrl_flag_s, ctrl_ap_flag, fal_write, ctrl_clr_flag, fsm_out, a_flag, input_sel, storage_out, reset_s, cntr_fl_rst, vga_fl_rst : std_logic;
+signal rng_color, return_color: std_logic_vector(1 downto 0);
 signal cntrl_data_in : std_logic_vector(2 downto 0);
 signal read_adr, write_adr : std_logic_vector(5 downto 0);
 signal seed : std_logic_vector(9 downto 0);
@@ -68,8 +68,29 @@ signal seed : std_logic_vector(9 downto 0);
 begin
 fsm: storage_fsm port map(clk, reset, a_flag, fsm_out);
 storage: bit_storage port map(clk, reset_s, write_adr , read_adr, storage_out);
-freg_2: flag_reg_2 port map(clk, reset, vga_s_fl, vga_clr_flag, return_color, vga_data, vga_flag_s);
-freg_3: flag_reg_3 port map(clk, reset, ctrl_s_fl, ctrl_clr_flag, cntrl_data_in, ctrl_data, ctrl_flag_s);
+
+
+--freg_2: flag_reg_2 port map(clk, reset, vga_s_fl, vga_clr_flag, return_color, vga_data, vga_flag_s);
+
+	--data_reg: gated_reg_2 port map(clk, reset, clr_flag, t, q);
+	vga_data_reg_0: gated_reg_1 port map(clk, reset, vga_clr_flag, return_color(0), vga_data(0));
+	vga_data_reg_1: gated_reg_1 port map(clk, reset, vga_clr_flag, return_color(1), vga_data(1));
+		
+	--
+
+vga_flag_reg: gated_reg_1 port map(clk, vga_fl_rst, vga_s_fl, '1', vga_flag_s);
+vga_fl_rst <= reset or vga_clr_flag;
+
+--
+
+--freg_3: flag_reg_3 port map(clk, reset, ctrl_s_fl, ctrl_clr_flag, cntrl_data_in, ctrl_data, ctrl_flag_s);
+cntr_data_reg_0: gated_reg_1 port map(clk, reset, ctrl_clr_flag, cntrl_data_in(0), ctrl_data(0));
+cntr_data_reg_1: gated_reg_1 port map(clk, reset, ctrl_clr_flag, cntrl_data_in(1), ctrl_data(1));
+cntr_data_reg_2: gated_reg_1 port map(clk, reset, ctrl_clr_flag, cntrl_data_in(2), ctrl_data(2));
+
+cntr_flag_reg: gated_reg_1 port map(clk, cntr_fl_rst, ctrl_s_fl, '1', ctrl_flag_s);
+cntr_fl_rst <= reset or ctrl_clr_flag;
+--
 hlatch: h_latch port map(vga_flag_s, fsm_out, input_sel);
 --seed_gen: random port map(clk, reset, game_rst, seed);
 --map_gen: mapgenerator port map(read_adr, seed, rng_color);
